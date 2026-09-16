8773e720f4055d40934105d4d3ddd194cbb69b29
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-09-16 22:02:12 +0530

    remove Lost & Found link from navbar

--- client/src/components/layout/NavBar.jsx
+++ client/src/components/layout/NavBar.jsx
@@ -27,7 +27,6 @@ function Navbar() {
 
   const navItems = [
     { to: "/home", label: "Home" },
-    { to: "/lost-found", label: "Lost & Found" },
     ...(isAdmin ? [{ to: "/admin", label: "Admin" }] : []),
     { to: "/profile", label: "My Profile" },
     { to: "/about", label: "About" }
